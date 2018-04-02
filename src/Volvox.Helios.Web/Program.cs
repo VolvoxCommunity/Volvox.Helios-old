3f1e0dde7dcfd3712dd76878a5ba493a00562386
Bill Chirico <billchirico@gmail.com>
2018-04-02 13:00:38 -0400

    Add Application Insights

--- src/Volvox.Helios.Web/Program.cs
+++ src/Volvox.Helios.Web/Program.cs
@@ -19,6 +19,7 @@ namespace Volvox.Helios.Web
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseApplicationInsights()
                 .UseStartup<Startup>()
                 .Build();
     }
