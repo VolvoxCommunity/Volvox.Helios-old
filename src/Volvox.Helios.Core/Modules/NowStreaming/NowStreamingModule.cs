db1244db2bc824396efcc16ce8a95541f9a1884d
DanielOfCourse <dre16978@gmail.com>
2018-05-29 23:14:53 -0500

    - Simplified logic around UpdateUser --- Method does its own validation, let it handle it. - Added XML styled comments for Update/Announce functions. - Added brackets on single line conditions. --- Mostly a style choice, but eh? - Removed unused Token variable.

--- src/Volvox.Helios.Core/Modules/NowStreaming/NowStreamingModule.cs
+++ src/Volvox.Helios.Core/Modules/NowStreaming/NowStreamingModule.cs
@@ -16,10 +16,9 @@ namespace Volvox.Helios.Core.Modules.NowStreaming
     public class NowStreamingModule : Module
     {
 
-        // Likely want this batched out to be read from a settings file?
+        // DRE: Likely want this batched out to be read from a settings file or be adjustable
+        // from Web level.
         private const ulong AnnounceChannelId = 392962633495740418;
-
-        private const string Token = "";
 
         private List<SocketGuildUser> StreamingList { get; } = new List<SocketGuildUser>();
 
@@ -43,17 +42,21 @@ namespace Volvox.Helios.Core.Modules.NowStreaming
             {
                 if (IsEnabled)
                 {
-                    if (guildUser.Game != null && guildUser.Game.Value.StreamType == StreamType.Twitch)
-                    {
-                        await this.AnnounceUser(guildUser);
-                    }
+                    await this.UpdateUser(guildUser);
                 }
             };
             
             return Task.CompletedTask;
         }
 
-        // [RequireUserPermission(GuildPermission.Administrator)]
+        // DRE: Pre-porting, this had "[RequireUserPermission(GuildPermission.Administrator)]"
+        // as an attribute tag- is that required/desired here? Not sure of OG context.
+
+        /// <summary>
+        /// Updates the user's roles if necessary, then announces their stream if they meet the role requirement
+        /// necessary to do so.
+        /// </summary>
+        /// <param name="user">User to be evaluated/adjusted for streaming announcement.</param>
         private async Task UpdateUser(SocketGuildUser user)
         {
             var role = user.Guild.Roles.FirstOrDefault(r => r.Name == "Non-Affiliate Streaming");
@@ -78,7 +81,9 @@ namespace Volvox.Helios.Core.Modules.NowStreaming
 
                     // Announce that the user is streaming.
                     if (user.Roles.All(r => r.Name != "Unranked"))
+                    {
                         await AnnounceUser(user);
+                    }
                 }
             }
 
@@ -95,10 +100,17 @@ namespace Volvox.Helios.Core.Modules.NowStreaming
 
                 // Remove user from the streaming list.
                 if (StreamingList.Any(u => u.Id == user.Id))
+                {
                     StreamingList.Remove(StreamingList.Single(u => u.Id == user.Id));
+                }
             }
         }
 
+        /// <summary>
+        /// Updates the user's roles if necessary, then announces their stream if they meet the role requirement
+        /// necessary to do so.
+        /// </summary>
+        /// <param name="user">User to be announced.</param>
         private async Task AnnounceUser(SocketGuildUser user)
         {
             // Build the embedded message.
