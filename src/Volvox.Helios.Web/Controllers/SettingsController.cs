e56b05db226fe12d43ced5900af2ac798899b6d7
Bill Chirico <billchirico@gmail.com>
2018-07-16 22:20:54 -0400

    Initial Stream Announcer Module Settings view, controller, and view model

---
+++ src/Volvox.Helios.Web/Controllers/SettingsController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Volvox.Helios.Service.Discord;
+using Volvox.Helios.Service.Extensions;
+using Volvox.Helios.Web.ViewModels.Settings;
+
+namespace Volvox.Helios.Web.Controllers
+{
+    public class SettingsController : Controller
+    {
+        // TODO: Return all of the modules
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // GET
+        public async Task<IActionResult> StreamAnnouncerSettings([FromServices] IDiscordUserService userService)
+        {
+            var guilds = await userService.GetUserGuilds();
+            
+            var viewModel = new StreamAnnouncerSettingsViewModel()
+            {
+                Guilds = new SelectList(guilds.FilterAdministrator(), "Id", "Name")
+            };
+            
+            return View(viewModel);
+        }
+        
+        // POST
+        [HttpPost]
+        public async Task<IActionResult> StreamAnnouncerSettings(StreamAnnouncerSettingsViewModel viewModel)
+        {
+            return Index();
+        }
+    }
+}
---
+++ src/Volvox.Helios.Web/ViewModels/Settings/StreamAnnouncerSettingsViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Volvox.Helios.Domain.Discord;
+
+namespace Volvox.Helios.Web.ViewModels.Settings
+{
+    [Authorize]
+    public class StreamAnnouncerSettingsViewModel
+    {
+        public SelectList Guilds { get; set; }
+        
+        [Required]
+        public ulong GuildId { get; set; }
+        
+        [Required]
+        public ulong ChannelId { get; set; }
+    }
+}
