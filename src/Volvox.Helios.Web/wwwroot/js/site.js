43eb657bc53254d46f9c1dc7f960f2b432fd8693
Bill Chirico <billchirico@gmail.com>
2018-07-20 00:16:04 -0400

    Convert javascript function to a jQuery plugin

--- src/Volvox.Helios.Web/wwwroot/js/site.js
+++ src/Volvox.Helios.Web/wwwroot/js/site.js
@@ -1,1 +1,23 @@
-﻿// Write your JavaScript code.
+﻿(function($) {
+
+    $.fn.populateGuildChannels = function(guildId) {
+        return this.each(function() {
+            let selectElement = $(this);
+
+            selectElement.empty();
+
+            selectElement.append('<option selected="true" disabled hidden>Select a channel</option>');
+            selectElement.prop('selectedIndex', 0);
+            selectElement.prop('disabled', false);
+
+            const url = `/Settings/GetGuildChannels?guildId=${guildId}`;
+
+            $.getJSON(url, function (data) {
+                $.each(data, function (key, entry) {
+                    selectElement.append($('<option></option>').attr('value', entry.id).text(entry.name));
+                })
+            });
+        });
+    };
+
+}(jQuery));
