6dfc3b2a055b628d1e95f00ebee0b4972d6d6e35
Bill Chirico <billchirico@gmail.com>
2018-07-25 11:49:32 -0400

    Login will now redirect directly to Discord Fix Settings navigation bar

--- src/Volvox.Helios.Web/Controllers/AuthenticationController.cs
+++ src/Volvox.Helios.Web/Controllers/AuthenticationController.cs
@@ -1,47 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authentication;
+﻿using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
-using Volvox.Helios.Web.Extensions;
 
 namespace Volvox.Helios.Web.Controllers
 {
     public class AuthenticationController : Controller
     {
         [HttpGet("~/signin")]
-        public async Task<IActionResult> SignIn() => View("SignIn", await HttpContext.GetExternalProviders());
-
-        [HttpPost("~/signin")]
-        public async Task<IActionResult> SignIn([FromForm] string provider)
+        public IActionResult SignIn()
         {
-            // Note: the "provider" parameter corresponds to the external
-            // authentication provider choosen by the user agent.
-            if (string.IsNullOrWhiteSpace(provider))
-            {
-                return BadRequest();
-            }
-
-            if (!await HttpContext.IsProviderSupported(provider))
-            {
-                return BadRequest();
-            }
-
-            // Instruct the middleware corresponding to the requested external identity
-            // provider to redirect the user agent to its own authorization endpoint.
-            // Note: the authenticationScheme parameter must match the value configured in Startup.cs
-            return Challenge(new AuthenticationProperties { RedirectUri = "/" }, provider);
+            return Challenge(new AuthenticationProperties {RedirectUri = "/"}, "Discord");
         }
 
-        [HttpGet("~/signout"), HttpPost("~/signout")]
+        [HttpGet("~/signout")]
+        [HttpPost("~/signout")]
         public IActionResult SignOut()
         {
             // Instruct the cookies middleware to delete the local cookie created
             // when the user agent is redirected from the external identity provider
-            // after a successful authentication flow (e.g Google or Facebook).
-            return SignOut(new AuthenticationProperties { RedirectUri = "/" },
+            // after a successful authentication flow.
+            return SignOut(new AuthenticationProperties {RedirectUri = "/"},
                 CookieAuthenticationDefaults.AuthenticationScheme);
         }
     }
--- src/Volvox.Helios.Web/Startup.cs
+++ src/Volvox.Helios.Web/Startup.cs
@@ -43,11 +43,9 @@ namespace Volvox.Helios.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            // GDPR Cookie Consent
             services.Configure<CookiePolicyOptions>(options =>
             {
-                // This lambda determines whether user consent for non-essential cookies 
-                // is needed for a given request.
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
