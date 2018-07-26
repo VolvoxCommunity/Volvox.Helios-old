50adf606a0924e9f0a1ef520a65eea14b3c10d4e
Gus Gray <agray1219@yahoo.com>
2018-07-25 22:27:06 -0700

    Added some basic test projects and a sample for each. One project for unit, one for integration.

---
+++ src/Tests/Tests.Unit/Volvox.Helios.Service/DiscordExtensionsTests.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using Volvox.Helios.Domain.Discord;
+using Volvox.Helios.Service.Extensions;
+using Xunit;
+
+namespace Tests.Unit.Volvox.Helios.Service
+{
+    /// <summary>
+    /// Q&D sample unit test class
+    /// </summary>
+    public class DiscordExtensionsTests
+    {
+        private const int adminPermission = 0x00000008;
+        private const int notAdminPermission = 0x00000000;
+
+        /// <summary>
+        /// Sample Test
+        /// </summary>
+        [Fact]
+        public void ReturnFilteredList()
+        {
+            //Arrange 
+            #region arrange
+            var TestGuilds = new List<Guild>
+            {
+                new Guild
+                {
+                    Id = 1,
+                    Name = "NotAdmin",
+                    Permissions = notAdminPermission
+                },
+
+
+                new Guild
+                {
+                    Id = 1,
+                    Name = "NotAdmin2",
+                    Permissions = notAdminPermission
+                },
+
+
+                new Guild
+                {
+                    Id = 1,
+                    Name = "Admin",
+                    Permissions = adminPermission
+                }
+            };
+            #endregion
+
+            //Act
+            var result = DiscordExtensions.FilterAdministrator(TestGuilds);
+
+            //Assert
+            Assert.True(result.Count == 1);
+            Assert.Equal("Admin", result[0].Name);
+        }
+    }
+}
