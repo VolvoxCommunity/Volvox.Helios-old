c527cfa9eb5c4cc14a91f35a302afe68b9482641
SolarysDev <strafe.solarys@gmail.com>
2018-07-30 12:01:19 -0700

    Document DiscordFacingContext

--- src/Volvox.Helios.Core/Modules/DiscordFacing/DiscordFacingContext.cs
+++ src/Volvox.Helios.Core/Modules/DiscordFacing/DiscordFacingContext.cs
@@ -5,10 +5,29 @@ namespace Volvox.Helios.Core.Modules.DiscordFacing
 {
     public class DiscordFacingContext
     {
+        /// <summary>
+        /// Emitted by DiscordFacingManager's bound client
+        /// </summary>
         public SocketUserMessage Message { get; }
+        
+        /// <summary>
+        /// Taken from Message
+        /// </summary>
         public ISocketMessageChannel Channel { get; }
+        
+        /// <summary>
+        /// Taken from Message
+        /// </summary>
         public SocketUser User { get; }
+        
+        /// <summary>
+        /// Fed by DiscordFacingManager
+        /// </summary>
         public DiscordSocketClient Client { get; }
+        
+        /// <summary>
+        /// The prefix the invoking guiild has set for the bot. Useful once Bapes finishes the settings framework.
+        /// </summary>
         public string GivenPrefix { get; }
 
         public DiscordFacingContext(SocketUserMessage m, DiscordSocketClient c, string p)
