65db8273502be7e0157c7ec1cd6a636d588be2b3
James Tombleson <luther38@gmail.com>
2018-09-08 22:04:38 -0700

    Mock up UI is more or less done.

--- src/Volvox.Helios.Web/Controllers/HomeController.cs
+++ src/Volvox.Helios.Web/Controllers/HomeController.cs
@@ -12,5 +12,7 @@ namespace Volvox.Helios.Web.Controllers
         public IActionResult Privacy() => View();
 
         public IActionResult About() => View();
+
+        public IActionResult Example() => View();
     }
 }
