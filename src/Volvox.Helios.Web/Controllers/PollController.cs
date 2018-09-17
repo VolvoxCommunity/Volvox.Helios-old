775432712dcf5f3c4870448662e6850636429df0
reece <consolelogreece@gmail.com>
2018-09-17 18:35:40 +0100

    Added new poll form

---
+++ src/Volvox.Helios.Web/Controllers/PollController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Volvox.Helios.Service.Discord.Guild;
+using Volvox.Helios.Web.Filters;
+using Volvox.Helios.Web.ViewModels.PollModule;
+
+namespace Volvox.Helios.Web.Controllers
+{
+    [Authorize]
+    [Route("/polls/{guildId}")]
+    [IsUserGuildAdminFilter]
+    public class PollController : Controller
+    {
+        #region Poll
+        [HttpGet("NewPoll")]
+        public async Task<IActionResult> NewPoll(ulong guildId, [FromServices] IDiscordGuildService guildService)
+        {
+            // All channels for guild.
+            var channels = await guildService.GetChannels(guildId);
+
+            // Text channels for guild.
+            var textChannels = channels.Where(x => x.Type == 0).ToList();
+
+            var viewModel = new NewPollViewModel
+            {
+                Channels = new SelectList(textChannels, "Id", "Name"),
+                TotOptions = 10,
+                GuildId = guildId.ToString()
+            };
+
+            return View(viewModel);
+        }
+
+        [HttpPost("NewPoll")]
+        public async Task<IActionResult> NewPoll(ulong guildId, NewPollViewModel viewModel, List<string> options)
+        {
+
+            var validOptions = options.Where(x => !string.IsNullOrWhiteSpace(x)).ToList();
+            
+            if (validOptions.Count < 2)
+            {
+                ModelState.AddModelError("All", "Please provide atleast 2 valid options.");
+                return View(viewModel);
+            }
+
+            if(!ModelState.IsValid) return View(viewModel);
+
+            return RedirectToAction("Index");
+        }
+        #endregion
+    }
+}
---
+++ src/Volvox.Helios.Web/ViewModels/Poll/NewPollViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Volvox.Helios.Web.ViewModels.PollModule
+{
+    public class NewPollViewModel
+    {
+        [Required]
+        [MaxLength(150)]
+        public string Title { get; set; }
+
+        public SelectList Channels { get; set; }
+
+        [Required]
+        [DisplayName("Channel")]
+        public ulong ChannelId { get; set; }
+
+        [Required]
+        public string GuildId { get; set; }
+
+        //Number of option text boxes to display in form.
+        [Required]
+        public int TotOptions { get; set; }
+    } 
+}
