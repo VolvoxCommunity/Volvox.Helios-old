a91966681d77bbf3ef7cb8f6bf4534ed8a27da4f
Bill Chirico <billchirico@gmail.com>
2018-09-18 19:14:39 -0400

    Fix merge conflicts

--- src/Volvox.Helios.Web/wwwroot/js/site.js
+++ src/Volvox.Helios.Web/wwwroot/js/site.js
@@ -1,39 +1,23 @@
 ﻿(function($) {
 
-    $.fn.populateGuildChannels = function(guildId) {
-        return this.each(function() {
-            const selectElement = $(this);
+  $.fn.populateGuildChannels = function(guildId) {
+    return this.each(function() {
+      const selectElement = $(this);
 
-            selectElement.empty();
+      selectElement.empty();
 
-            selectElement.append('<option selected="true" disabled hidden>Select a channel</option>');
-            selectElement.prop("selectedIndex", 0);
-            selectElement.prop("disabled", false);
+      selectElement.append('<option selected="true" disabled hidden>Select a channel</option>');
+      selectElement.prop("selectedIndex", 0);
+      selectElement.prop("disabled", false);
 
-            const url = `/api/GetGuildChannels?guildId=${guildId}`;
+      const url = `/api/GetGuildChannels?guildId=${guildId}`;
 
-            $.getJSON(url, function (data) {
-                $.each(data, function (key, entry) {
-                    selectElement.append($("<option></option>").attr("value", entry.id).text(entry.name));
-                });
-            });
+      $.getJSON(url, function (data) {
+        $.each(data, function (key, entry) {
+          selectElement.append($("<option></option>").attr("value", entry.id).text(entry.name));
         });
-    };
-
-    $.fn.populateSettings = function() {
-        return this.each(function() {
-            const element = $(this);
-            
-            const url = "/api/GetUserAdminGuilds";
-
-            $.getJSON(url, function (data) {
-                element.empty();
-                
-                $.each(data, function (key, entry) {
-                    element.append($('<a class="dropdown-item"></a>').attr("href", `/Settings/${entry.id}`).text(entry.name));
-                });
-            });
-        });
+      });
+    });
   };
 
   $.fn.populateGuildsInBot = function () {
@@ -51,5 +35,40 @@
       });
     });
   };
+
+  $.fn.populateSettings = function () {
+    return this.each(function () {
+      const element = $(this);
+
+      const url = "/api/GetUserAdminGuilds";
+
+      $.getJSON(url, function (data) {
+        element.empty();
+
+        $.each(data, function (key, entry) {
+          element.append(generateGuildDropdownItem(entry.id, entry.name, entry.icon));
+        });
+      });
+    });
+  };
+
+  function generateGuildDropdownItem(guildId, guildName, guildIcon) {
+
+    // guildIcon will be null if the guild doesn't have an icon. Therefore set src to default error icon.
+    const iconUrl = guildIcon == null ? "/images/error.png" : `https://cdn.discordapp.com/icons/${guildId}/${guildIcon}.png`;
+
+    return (`
+      <div>
+        <a class="dropdown-item dropdown-item-container" href="/Settings/${guildId}">
+            <span>
+                <img class="dropdown-image-small" src="${iconUrl}">
+            </span>
+            <span>
+                ${guildName}
+            </span>
+        </a>
+    </div>
+    `)
+  }
 
 }(jQuery));
