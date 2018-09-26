5de05a14954e80c50b8f202148a55d2edc8a69e3
reece <consolelogreece@gmail.com>
2018-09-26 21:11:01 +0100

    Changed database to allow persisting stream messages

---
+++ src/Volvox.Helios.Domain/Module/StreamAnnouncerMessages.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Volvox.Helios.Domain.ModuleSettings;
+
+namespace Volvox.Helios.Core.Modules.StreamAnnouncer
+{
+    public class StreamAnnouncerMessages
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public ulong UserId { get; set; }
+
+        public ulong MessageId { get; set; }
+
+        public ulong ChannelId { get; set; }
+
+        public virtual StreamAnnouncerSettings StreamAnnouncerSettings { get; set; }
+    }
+}
--- src/Volvox.Helios.Domain/ModuleSettings/StreamAnnouncerSettings.cs
+++ src/Volvox.Helios.Domain/ModuleSettings/StreamAnnouncerSettings.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using Volvox.Helios.Core.Modules.StreamAnnouncer;
 using Volvox.Helios.Domain.Module;
 
 namespace Volvox.Helios.Domain.ModuleSettings
@@ -6,5 +7,7 @@ namespace Volvox.Helios.Domain.ModuleSettings
     public class StreamAnnouncerSettings : ModuleSettings
     {
         public List<StreamAnnouncerChannelSettings> ChannelSettings { get; set; }
+
+        public List<StreamAnnouncerMessages> StreamMessages { get; set; }
     }
 }
