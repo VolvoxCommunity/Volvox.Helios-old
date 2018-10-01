d0d0c086db6f1de1c861ed3849a085879f650fcb
Bill Chirico <billchirico@gmail.com>
2018-09-30 22:49:46 -0400

    Code cleanup

--- src/Volvox.Helios.Core/Modules/Command/Framework/ICommand.cs
+++ src/Volvox.Helios.Core/Modules/Command/Framework/ICommand.cs
@@ -1,6 +1,6 @@
 using System.Threading.Tasks;
 
-namespace Volvox.Helios.Core.Modules.DiscordFacing.Framework
+namespace Volvox.Helios.Core.Modules.Command.Framework
 {
     public interface ICommand
     {
