6543b2fbdd3519fefa51e56d4c27977dff2c4ec9
reece <consolelogreece@gmail.com>
2018-10-15 14:36:59 +0100

    added initial moderation controller

---
+++ src/Volvox.Helios.Web/Controllers/ModerationController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Volvox.Helios.Domain.Module.ModerationModule.ProfanityFilter;
+using Volvox.Helios.Domain.ModuleSettings;
+using Volvox.Helios.Service.EntityService;
+using Volvox.Helios.Service.ModuleSettings;
+
+namespace Volvox.Helios.Web.Controllers
+{
+    public class ModerationController : Controller
+    {
+        IModuleSettingsService<ModerationSettings> _moderationSettings;
+
+        IEntityService<ProfanityFilter> _entityServiceProfanityFilter;
+
+        public ModerationController(IModuleSettingsService<ModerationSettings> moderationSettings, IEntityService<ProfanityFilter> entityServiceProfanityFilter)
+        {
+            _moderationSettings = moderationSettings;
+
+            _entityServiceProfanityFilter = entityServiceProfanityFilter;
+        }
+
+        public async Task<IActionResult> Index(ulong guildId)
+        {
+
+        }
+    }
+}
