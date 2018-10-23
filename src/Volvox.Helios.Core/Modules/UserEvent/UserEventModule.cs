3ceeaa7b855974367a28e85e8923c626667a36bf
Bill Chirico <billchirico@gmail.com>
2018-10-22 21:41:48 -0400

    Add user left handler Add UserEvent migration

--- src/Volvox.Helios.Core/Modules/UserEvent/UserEventModule.cs
+++ src/Volvox.Helios.Core/Modules/UserEvent/UserEventModule.cs
@@ -5,20 +5,26 @@ using Microsoft.Extensions.Logging;
 using Volvox.Helios.Core.Modules.Common;
 using Volvox.Helios.Core.Modules.StreamerRole;
 using Volvox.Helios.Core.Utilities;
+using Volvox.Helios.Domain.ModuleSettings;
+using Volvox.Helios.Service.ModuleSettings;
 
 namespace Volvox.Helios.Core.Modules.UserEvent
 {
     public class UserEventModule : Module
     {
+        private readonly IModuleSettingsService<UserEventSettings> _settingsService;
+
         /// <summary>
         ///     Perform actions based on a user event.
         /// </summary>
         /// <param name="discordSettings">Settings used to connect to Discord.</param>
         /// <param name="logger">Logger.</param>
         /// <param name="config">Application configuration.</param>
+        /// <param name="settingsService">Settings service.</param>
         public UserEventModule(IDiscordSettings discordSettings, ILogger<StreamerRoleModule> logger,
-            IConfiguration config) : base(discordSettings, logger, config)
+            IConfiguration config, IModuleSettingsService<UserEventSettings> settingsService) : base(discordSettings, logger, config)
         {
+            _settingsService = settingsService;
         }
 
         /// <summary>
@@ -27,7 +33,29 @@ namespace Volvox.Helios.Core.Modules.UserEvent
         /// <param name="client">Client that the module will be registered to.</param>
         public override Task Init(DiscordSocketClient client)
         {
+            client.UserLeft += async user =>
+            {
+                var settings = await _settingsService.GetSettingsByGuild(user.Guild.Id);
+
+                if (settings.EnableUserLeftEvent)
+                {
+                    HandleUserLeftEvent(user, settings.UserLeftEventChannelId);
+                }
+            };
+
             return Task.CompletedTask;
+        }
+
+        /// <summary>
+        /// Handle when a user leaves a guild.
+        /// </summary>
+        /// <param name="user">User that left.</param>
+        /// <param name="announceChannelId">Channel to announce the event to.</param>
+        private static async void HandleUserLeftEvent(SocketGuildUser user, ulong announceChannelId)
+        {
+            var channel = user.Guild.GetTextChannel(announceChannelId);
+
+            await channel.SendMessageAsync($"{user.Username} has left the server.");
         }
     }
 }
---
+++ src/Volvox.Helios.Service/Migrations/20181023014101_UserEventSettings.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Volvox.Helios.Service.Migrations
+{
+    public partial class UserEventSettings : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserEventSettings",
+                columns: table => new
+                {
+                    GuildId = table.Column<decimal>(nullable: false),
+                    Enabled = table.Column<bool>(nullable: false),
+                    EnableUserLeftEvent = table.Column<bool>(nullable: false),
+                    UserLeftEventChannelId = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserEventSettings", x => x.GuildId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserEventSettings");
+        }
+    }
+}
