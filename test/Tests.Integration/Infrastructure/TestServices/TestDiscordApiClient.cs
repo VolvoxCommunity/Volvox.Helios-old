b145968b2a320b6d9ffcd4fe7615288606a21d98
Gus Gray <agray1219@yahoo.com>
2018-10-23 22:22:40 -0700

    fixed DI issue with TestDiscordApiClient

--- test/Tests.Integration/Infrastructure/TestServices/TestDiscordApiClient.cs
+++ test/Tests.Integration/Infrastructure/TestServices/TestDiscordApiClient.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Volvox.Helios.Service.Clients;
 
@@ -6,6 +7,14 @@ namespace Tests.Integration.Infrastructure.TestServices
 {
     public class TestDiscordApiClient : IDiscordAPIClient
     {
+        private readonly HttpClient _fakeClient; //TODO: probably remove this, or do some different DI
+
+        public TestDiscordApiClient(HttpClient client)
+        {
+            //Needed for the DI from addhttpclient()
+            _fakeClient = client;
+        }
+
         public Task<string> GetGuildChannels(ulong guildId)
         {
             return Task.FromResult("[{\"id\":\"468467000344313866\",\"name\":\"Volvox\",\"icon\":\"72225e0911dc450048250a6b28dfab5b\"},{\"id\":\"471503658694213632\",\"name\":\"BapesTestServer\",\"icon\":null}]");
