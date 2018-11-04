050e046637f7dcafc40d936174546af5f2a00a76
reece <consolelogreece@gmail.com>
2018-11-04 22:56:44 +0000

    initial front end for moderator module

--- src/Volvox.Helios.Web/Controllers/ModerationController.cs
+++ src/Volvox.Helios.Web/Controllers/ModerationController.cs
@@ -1,15 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Volvox.Helios.Domain.Module.ModerationModule.ProfanityFilter;
 using Volvox.Helios.Domain.ModuleSettings;
 using Volvox.Helios.Service.EntityService;
 using Volvox.Helios.Service.ModuleSettings;
+using Volvox.Helios.Web.Filters;
 
 namespace Volvox.Helios.Web.Controllers
 {
+    [Authorize]
+    [Route("/moderator/{guildId}")]
+    [IsUserGuildAdminFilter]
     public class ModerationController : Controller
     {
         IModuleSettingsService<ModerationSettings> _moderationSettings;
@@ -23,9 +25,9 @@ namespace Volvox.Helios.Web.Controllers
             _entityServiceProfanityFilter = entityServiceProfanityFilter;
         }
 
-        //public async Task<IActionResult> Index(ulong guildId)
-        //{
-
-        //}
+        public async Task<IActionResult> Index(ulong guildId)
+        {
+            return View();
+        }
     }
 }
