0558d27fa2088eba2783b3872ea7b54388e867e2
reece <consolelogreece@gmail.com>
2018-11-17 14:07:37 +0000

    added foundations for punishments settings page

---
+++ src/Volvox.Helios.Web/Models/Moderation/PunishmentModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Volvox.Helios.Domain.Module.ModerationModule.Common;
+
+namespace Volvox.Helios.Web.Models.Moderation
+{
+    public class PunishmentModel
+    {
+        public short WarningThreshold { get; set; }
+
+        public WarningType WarningType { get; set; }
+
+        public PunishType PunishType { get; set; }
+
+        public int PunishDuration { get; set; }
+
+        public ulong RoleId { get; set; }
+    }
+}
---
+++ src/Volvox.Helios.Web/ViewModels/Moderation/PunishmentsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Volvox.Helios.Web.Models.Moderation;
+
+namespace Volvox.Helios.Web.ViewModels.Moderation
+{
+    public class PunishmentsViewModel
+    {
+        public List<PunishmentModel> Punishments { get; set; }
+    }
+}
