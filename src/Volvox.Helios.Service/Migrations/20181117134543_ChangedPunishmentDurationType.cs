0eab9a312931751431018bc1285bf531e9e72487
reece <consolelogreece@gmail.com>
2018-11-17 14:06:18 +0000

    changed punishment duration type

---
+++ src/Volvox.Helios.Service/Migrations/20181117134543_ChangedPunishmentDurationType.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Volvox.Helios.Service.Migrations
+{
+    public partial class ChangedPunishmentDurationType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "PunishDuration",
+                table: "mod_punishments",
+                nullable: true,
+                oldClrType: typeof(short),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<short>(
+                name: "PunishDuration",
+                table: "mod_punishments",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldNullable: true);
+        }
+    }
+}
