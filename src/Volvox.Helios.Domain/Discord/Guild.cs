2a0e5bcdd1ab7e9dae35ce9cb05b6853c62aa446
Bill Chirico <billchirico@gmail.com>
2018-11-20 00:11:50 -0500

    Initialize GuildDetails from Guild model constructor Dashboard controller cleanup

--- src/Volvox.Helios.Domain/Discord/Guild.cs
+++ src/Volvox.Helios.Domain/Discord/Guild.cs
@@ -1,9 +1,12 @@
-﻿using System.Collections.Generic;
-
-namespace Volvox.Helios.Domain.Discord
+﻿namespace Volvox.Helios.Domain.Discord
 {
     public class Guild
     {
+        public Guild()
+        {
+            Details = new GuildDetails();
+        }
+
         public ulong Id { get; set; }
 
         public string Name { get; set; }
@@ -12,6 +15,9 @@ namespace Volvox.Helios.Domain.Discord
 
         public string ImageUrl { get; set; }
 
-        public GuildDetails Details { get; set; }
+        /// <summary>
+        ///     Guild specific details. NOT populated from Discord API.
+        /// </summary>
+        public GuildDetails Details { get; }
     }
 }
--- src/Volvox.Helios.Web/Controllers/DashboardController.cs
+++ src/Volvox.Helios.Web/Controllers/DashboardController.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Volvox.Helios.Core.Bot;
-using Volvox.Helios.Domain.Discord;
-using Volvox.Helios.Service.Discord.Guild;
 using Volvox.Helios.Service.Discord.UserGuild;
 using Volvox.Helios.Service.Extensions;
 using Volvox.Helios.Web.ViewModels.Dashboard;
@@ -17,12 +12,10 @@ namespace Volvox.Helios.Web.Controllers
     public class DashboardController : Controller
     {
         private readonly IDiscordUserGuildService _userGuildService;
-        private readonly IDiscordGuildService _guildService;
 
-        public DashboardController(IDiscordUserGuildService userGuildService, IDiscordGuildService guildService)
+        public DashboardController(IDiscordUserGuildService userGuildService)
         {
             _userGuildService = userGuildService;
-            _guildService = guildService;
         }
 
 
@@ -30,18 +23,14 @@ namespace Volvox.Helios.Web.Controllers
         {
             var guilds = await _userGuildService.GetUserGuilds();
 
-            // Populate guild details
+            // Populate guild details.
             foreach (var guild in guilds)
             {
-                guild.Guild.Details = new GuildDetails
-                {
-                    IsBotInGuild = bot.IsBotInGuild(guild.Guild.Id)
-                };
+                guild.Guild.Details.IsBotInGuild = bot.IsBotInGuild(guild.Guild.Id);
 
+                // Bot must be in the guild to retrieve details.
                 if (guild.Guild.Details.IsBotInGuild)
-                {
                     guild.Guild.Details.MemberCount = bot.GetGuild(guild.Guild.Id).MemberCount;
-                }
             }
 
             var viewModel = new DashboardIndexViewModel
