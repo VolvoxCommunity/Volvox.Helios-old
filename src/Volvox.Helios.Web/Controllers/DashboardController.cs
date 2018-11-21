f7e72f460a933736888dd55153bf79cff10486ff
Bill Chirico <billchirico@gmail.com>
2018-11-20 19:45:50 -0500

    Add initial Dashboard Details

--- src/Volvox.Helios.Web/Controllers/DashboardController.cs
+++ src/Volvox.Helios.Web/Controllers/DashboardController.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Volvox.Helios.Core.Bot;
@@ -12,30 +14,55 @@ namespace Volvox.Helios.Web.Controllers
     public class DashboardController : Controller
     {
         private readonly IDiscordUserGuildService _userGuildService;
+        private readonly IBot _bot;
 
-        public DashboardController(IDiscordUserGuildService userGuildService)
+        public DashboardController(IDiscordUserGuildService userGuildService, IBot bot)
         {
             _userGuildService = userGuildService;
+            _bot = bot;
         }
 
-
-        public async Task<IActionResult> Index([FromServices] IBot bot)
+        public async Task<IActionResult> Index()
         {
             var guilds = await _userGuildService.GetUserGuilds();
 
             // Populate guild details.
             foreach (var guild in guilds)
             {
-                guild.Guild.Details.IsBotInGuild = bot.IsBotInGuild(guild.Guild.Id);
+                guild.Guild.Details.IsBotInGuild = _bot.IsBotInGuild(guild.Guild.Id);
 
                 // Bot must be in the guild to retrieve details.
                 if (guild.Guild.Details.IsBotInGuild)
-                    guild.Guild.Details.MemberCount = bot.GetGuild(guild.Guild.Id).MemberCount;
+                    guild.Guild.Details.MemberCount = _bot.GetGuild(guild.Guild.Id).MemberCount;
             }
 
             var viewModel = new DashboardIndexViewModel
             {
                 UserGuilds = guilds.FilterAdministrator()
+            };
+
+            return View(viewModel);
+        }
+
+        [Route("Dashboard/{guildId}")]
+        public async Task<IActionResult> Details(ulong guildId)
+        {
+            var guilds = await _userGuildService.GetUserGuilds();
+
+            // Populate guild details.
+            foreach (var guild in guilds)
+            {
+                guild.Guild.Details.IsBotInGuild = _bot.IsBotInGuild(guild.Guild.Id);
+
+                // Bot must be in the guild to retrieve details.
+                if (guild.Guild.Details.IsBotInGuild)
+                    guild.Guild.Details.MemberCount = _bot.GetGuild(guild.Guild.Id).MemberCount;
+            }
+
+            var viewModel = new DashboardDetailsViewModel
+            {
+                UserGuilds = guilds.FilterAdministrator(),
+                Guild = guilds.FirstOrDefault(g => g.Guild.Id == guildId)?.Guild
             };
 
             return View(viewModel);
