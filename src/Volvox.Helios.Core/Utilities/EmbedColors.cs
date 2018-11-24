96e41e168ae0ca78480638f3a0b44ee55466150c
Solarys <strafe.solarys@gmail.com>
2018-11-24 08:58:38 -0800

    add about command + give help command a fancy hat

---
+++ src/Volvox.Helios.Core/Utilities/EmbedColors.cs
@@ -0,0 +1,10 @@
+using Discord;
+
+namespace Volvox.Helios.Core.Utilities
+{
+    public static class EmbedColors
+    {
+        public static Color LogoColor = new Color(102, 204, 51);
+        public static Color Twitch = new Color(100, 65, 165);
+    }
+}
