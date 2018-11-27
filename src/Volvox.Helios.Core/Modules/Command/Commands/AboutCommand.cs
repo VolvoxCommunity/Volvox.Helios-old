bb315401af9df825db825f5cff139e63789690d1
Bill Chirico <billchirico@gmail.com>
2018-11-27 17:31:20 -0500

    Add bot version Remove redundant cases

--- src/Volvox.Helios.Core/Modules/Command/Commands/AboutCommand.cs
+++ src/Volvox.Helios.Core/Modules/Command/Commands/AboutCommand.cs
@@ -12,19 +12,23 @@ namespace Volvox.Helios.Core.Modules.Command.Commands
     {
         public async Task TryTrigger(CommandContext context)
         {
-            if(!context.Message.Content.StartsWith($"{context.GivenPrefix}about")) throw new TriggerFailException();
+            if (!context.Message.Content.StartsWith($"{context.GivenPrefix}about")) throw new TriggerFailException();
             await Execute(context);
         }
 
         public async Task Execute(CommandContext context)
         {
             if (!( context.Channel is SocketGuildChannel channel )) return;
+
             // Your args will always start at 1; the command itself is argument zero.
-            string flag = context.Message.Content.Split(' ')[1];
+            var flag = context.Message.Content.Split(' ')[1];
+
             var embed = new EmbedBuilder()
                 .WithColor(EmbedColors.LogoColor)
                 .WithAuthor(new EmbedAuthorBuilder()
                     .WithName("Helios"));
+
+            var version = Assembly.GetEntryAssembly().GetName().Version;
 
             switch (flag)
             {
@@ -36,21 +40,14 @@ namespace Volvox.Helios.Core.Modules.Command.Commands
                         .AddField("User Count", channel.Guild.MemberCount)
                         .AddField("Voice Region", channel.Guild.VoiceRegionId, true);
                     break;
-                case "-b":
-                case "-bot":
-                case null:
-                    embed.WithTitle("So you want to hear my life story, ey?")
-                        .AddField(".NET Core Runtime Version", Assembly.GetEntryAssembly().GetName().Version)
-                        .AddField("Bloat Factor", $"{GC.GetTotalMemory(false) / 1048576}MB", true)
-                        .AddField("Server Count", context.Client.Guilds.Count, true);
-                    await context.Channel.SendMessageAsync("", false, embed.Build());
-                    break;
                 default:
                     embed.WithTitle("So you want to hear my life story, ey?")
-                        .AddField(".NET Core Runtime Version", Assembly.GetEntryAssembly().GetName().Version)
+                        .AddField("Bot Version", $"v{version.Major}.{version.Minor}.{version.Build}")
                         .AddField("Bloat Factor", $"{GC.GetTotalMemory(false) / 1048576}MB", true)
                         .AddField("Server Count", context.Client.Guilds.Count, true)
-                        .WithFooter(new EmbedFooterBuilder().WithText("psst, you can use -g with this command to get server information!"));
+                        .WithFooter(
+                            new EmbedFooterBuilder().WithText(
+                                "psst, you can use -g with this command to get server information!"));
                     await context.Channel.SendMessageAsync("", false, embed.Build());
                     break;
             }
