caaae7ca3216f1f9addca04720eb65801aece4a7
reece <consolelogreece@gmail.com>
2018-12-23 12:02:55 +0000

    added field to store job id for active punishment removal

---
+++ src/Volvox.Helios.Service/Migrations/20181223104311_AddedHangfireJobIdFieldActivePunishment.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Volvox.Helios.Service.Migrations
+{
+    public partial class AddedHangfireJobIdFieldActivePunishment : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "JobId",
+                table: "mod_ActivePunishments",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "JobId",
+                table: "mod_ActivePunishments");
+        }
+    }
+}
