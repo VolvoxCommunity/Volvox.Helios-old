307e6d180145024375421323106fcdcafb91b296
reece <consolelogreece@gmail.com>
2019-01-07 15:17:48 +0000

    updated job to only take the id of the active punishment. this is so we can modify the active punishment and hangfire won't need to worry about it.

--- src/Volvox.Helios.Core/Jobs/RemovePunishmentJob.cs
+++ src/Volvox.Helios.Core/Jobs/RemovePunishmentJob.cs
@@ -1,8 +1,10 @@
 ﻿using System.Threading.Tasks;
+using Hangfire.Server;
 using Microsoft.Extensions.DependencyInjection;
 using Volvox.Helios.Core.Modules.ModerationModule.PunishmentService;
 using Volvox.Helios.Domain.Module.ModerationModule.Common;
 using Volvox.Helios.Service.BackgroundJobs;
+using Volvox.Helios.Service.EntityService;
 
 namespace Volvox.Helios.Core.Jobs
 {
@@ -21,17 +23,29 @@ namespace Volvox.Helios.Core.Jobs
             _punishmentService = punishmentService;
         }
 
-        public string SchedulePunishmentRemoval(ActivePunishment punishment)
+        public string ScheduleActivePunishmentRemoval(ActivePunishment activePunishment)
         {
-            using (var scope = _scopeFactory.CreateScope())
-            {
-                return _jobService.ScheduleJob(() => RemovePunishmentDiscord(punishment), punishment.PunishmentExpires);
-            }
+            return _jobService.ScheduleJob(() => RemoveActivePunishmentDiscord(activePunishment.Id), activePunishment.PunishmentExpires);
         }
 
-        public async Task RemovePunishmentDiscord(ActivePunishment punishment)
+        public async Task RemoveActivePunishmentDiscord(int activePunishmentId)
         {
-            await _punishmentService.RemovePunishment(punishment);
+            ActivePunishment activePunishment;
+            
+            using (var scope = _scopeFactory.CreateScope())
+            {
+                var activePunishmentService = scope.ServiceProvider.GetRequiredService<IEntityService<ActivePunishment>>();
+
+                activePunishment = await activePunishmentService.GetFirst(p => p.Id == activePunishmentId, p => p.User);
+            }
+
+            // Active Punishment has already been removed by guild admin.
+            if (activePunishment == null) return;
+
+            await _punishmentService.RemovePunishment(activePunishment);
         }
     }
 }
+
+
+
