c7105c5a7e9cee444a793b5fcce6dd4c8b5f7425
codingsamuel <codingsamuel@moutinho.org>
2019-01-09 15:48:07 +0100

    added reaction role view model

---
+++ src/Volvox.Helios.Web/ViewModels/Settings/ReactionRoleSettingsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using Volvox.Helios.Domain.Module;
+
+namespace Volvox.Helios.Web.ViewModels.Settings
+{
+    public class ReactionRoleSettingsViewModel
+    {
+        [Required]
+        [DisplayName("Enabled")]
+        [DefaultValue(true)]
+        public bool Enabled { get; set; }
+    }
+}
