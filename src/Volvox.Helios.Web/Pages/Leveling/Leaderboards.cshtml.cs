ecce960f20e5c4b824b4175e1b97c3b39a81c6e7
codingsamuel <codingsamuel@moutinho.org>
2019-01-23 21:00:18 +0100

    added Razor Leaderboards page added viewimports

---
+++ src/Volvox.Helios.Web/Pages/Leveling/Leaderboards.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Volvox.Helios.Web.ViewModels.Settings;
+
+namespace Volvox.Helios.Web.Pages.Leveling
+{
+    public class LeaderboardsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
