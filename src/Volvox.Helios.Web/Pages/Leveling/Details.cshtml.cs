d7528ce0aec5508151766624a2138dcc8544a2bc
codingsamuel <codingsamuel@moutinho.org>
2019-01-28 21:41:38 +0100

    added user details page

---
+++ src/Volvox.Helios.Web/Pages/Leveling/Details.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Volvox.Helios.Web.Pages.Leveling
+{
+    public class DetailsModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
