f75e2b8ee47ed0003422c7748347a04768b2c101
codingsamuel <codingsamuel@moutinho.org>
2019-02-23 19:39:14 +0100

    added view model and view to clean chat

---
+++ src/Volvox.Helios.Web/ViewModels/Settings/CleanChatSettingsViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Volvox.Helios.Web.ViewModels.Settings
+{
+    public class CleanChatSettingsViewModel
+    {
+        [Required]
+        [DisplayName("Enabled")]
+        [DefaultValue(true)]
+        public bool Enabled { get; set; }
+
+        public SelectList Channels { get; set; }
+
+        public int MessageDuration { get; set; }
+    }
+}
